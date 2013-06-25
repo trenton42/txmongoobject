f1a09e0a4fb6508c99108207a3e9f6f6c5941749
Trenton Broughton <trenton@devpie.com>
2013-06-25 15:32:06 -0400

    Initial import

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='txmongoobject',
+      version='0.1',
+      description='Object models in Mongo',
+      long_description=readme(),
+      keywords='mongo twisted',
+      url='http://github.com/trenton42/txmongoobject',
+      author='Trenton Broughton',
+      author_email='trenton@devpie.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'twisted',
+          'txmongo'
+      ],
+      zip_safe=False)
