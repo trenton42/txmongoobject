48ae5ec4c332f222b24d930623af274ce523e9f2
Trenton Broughton <trenton@devpie.com>
2013-08-30 11:36:45 -0400

    MOAR MOAR MOAR

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,12 +7,18 @@ from bson.objectid import ObjectId
 
 model.MongoObj.dbname = 'test_database'
 
+class Fragment(model.MongoObj):
+	testValue = model.stringProperty()
+
+
 class CollectionObject(model.MongoObj):
 	testString = model.stringProperty()
 	testInt = model.intProperty()
 	testFloat = model.floatProperty()
 	testBool = model.boolProperty()
 	testDate = model.dateProperty()
+	testRef = model.referenceProperty(Fragment)
+	testRefList = model.listProperty(wrapper=model.referenceProperty(Fragment))
 
 
 class TestCollection(unittest.TestCase):
@@ -36,6 +42,54 @@ class TestCollection(unittest.TestCase):
 		self.assertIdentical(col.testFloat, None)
 		self.assertIdentical(col.testDate, None)
 
+	@defer.inlineCallbacks
+	def test_reference(self):
+		col = CollectionObject()
+		frag = Fragment()
+		frag.testValue = 'This is a test'
+
+		yield frag.save()
+
+		col.testRef = frag
+
+		yield col.save()
+
+		self.assertEqual(col.testRef, frag)
+
+		newobj = CollectionObject()
+		yield newobj.load(col._id)
+
+		self.assertEqual(newobj.testRef, frag)
+
+	@defer.inlineCallbacks
+	def test_load(self):
+		col = CollectionObject()
+		yield col.save()
+
+		frag = Fragment()
+		yield frag.save()
+
+		newcol = yield CollectionObject.findOne(col._id)
+
+		self.assertTrue(newcol.loaded)
+
+		self.assertEqual(col, newcol)
+
+
+	@defer.inlineCallbacks
+	def test_comparison(self):
+		col = CollectionObject()
+		yield col.save()
+
+		dup = CollectionObject()
+		yield dup.load(col._id)
+		
+		other = CollectionObject()
+		yield other.save()
+
+		self.assertNotEqual(col, other)
+		self.assertEqual(col, dup)
+		self.assertEqual(col, dup._id)
 
 	@defer.inlineCallbacks
 	def test_dirty(self):
@@ -60,12 +114,18 @@ class TestCollection(unittest.TestCase):
 		self.assertEqual(newobj._id, col._id)
 
 		newobj.testBool = True
+
+		for i in ('testString', 'testDate', 'testFloat', 'testRef', 'testInt', 'testRefList'):
+			# Set all attributes to the same value. Should not set the dirty flag
+			setattr(newobj, i, getattr(newobj, i))
 		
 		self.assertIn('testBool', newobj._prop_dirty)
 		self.assertNotIn('testString', newobj._prop_dirty)
 		self.assertNotIn('testDate', newobj._prop_dirty)
 		self.assertNotIn('testFloat', newobj._prop_dirty)
 		self.assertNotIn('testInt', newobj._prop_dirty)
+		self.assertNotIn('testRef', newobj._prop_dirty)
+		self.assertNotIn('testRefList', newobj._prop_dirty)
 
 		yield newobj.save()
 
