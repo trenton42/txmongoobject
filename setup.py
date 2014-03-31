e7a80c4fe13c82c87e1c84dfa8ba4db2ac8ff110
Trenton Broughton <trenton@devpie.com>
2014-03-31 12:46:34 -0400

    Some tweaks

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='txmongoobject',
-      version='0.2.3',
+      version='0.2.4',
       description='Object models in Mongo',
       long_description=readme(),
       keywords='mongo twisted',
@@ -16,7 +16,8 @@ setup(name='txmongoobject',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'twisted',
-          'txmongo'
+          'txmongo',
+          'pymongo',
+          'pytz'
       ],
       zip_safe=False)
